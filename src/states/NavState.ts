7c858a6313b9518466afffcc94aa0261215924bb
inseo <wisberry2022@gmail.com>
2024-01-20 10:56:35 +0900

    불필요한 state 제거

--- src/states/NavState.ts
+++
@@ -1,9 +0,0 @@
-import { nav } from "@/constants/Nav";
-import { atom } from "recoil";
-
-const defaultTab = Object.keys(nav).find(data => nav[data].isDefault);
-
-export const CurrentTab = atom<string>({
-  key: "CurrentTab",
-  default: defaultTab,
-})
