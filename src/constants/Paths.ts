80fbc8e9bdd0197544930f4b547e8c07e4ef43d0
inseo <wisberry2022@gmail.com>
2024-01-21 18:03:59 +0900

    직위 일괄등록을 위한 Path, Service, Domain Type 지정

--- src/constants/Paths.ts
+++ src/constants/Paths.ts
@@ -4,4 +4,7 @@ export const Paths = {
     bulkRegister: "/fundamental/qualified/bulk",
     register: "/fundamental/qualified/staff",
   },
+  position: {
+    bulkRegister: "/fundamentla/position/bulk",
+  },
 };
